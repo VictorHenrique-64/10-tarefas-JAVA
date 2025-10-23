3e47264262f66154707ef56967f89add2f59497a
VictorHenrique-64 <victor.vh64@outlook.com>
2025-10-23 18:58:19 -0300

    2 - Calculadora simples
    atualização

--- 2- Calculadora Simples/src/Main.java
+++ 2- Calculadora Simples/src/Main.java
@@ -4,47 +4,100 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("\n \\~\\~\\~\\~\\ CALCULADORA SIMPLES /~/~/~/~/");
+        System.out.println("\n \\~\\~\\~\\~\\ CALCULADORA SIMPLES /~/~/~/~/\n");
 
-        char[] operacao = {'+', '-', 'x', '÷'};
         char repetir;
+        int num1 = 0;
+        int num2 = 0;
+        boolean validacao = false;
+        int operacao = 0;
 
         do {
-            System.out.print("__ __ __ = __");
-            System.out.print("Digite o primeiro número: ");
-            int num1 = scan.nextInt();
+            while (!validacao) {
+                System.out.print("Digite o primeiro número: ");
+                if (scan.hasNextInt()) {
+                    num1 = scan.nextInt();
+                    validacao = true;
+                } else {
+                    System.out.println("\n    * O valor inserido deve ser um número inteiro *\n");
+                    scan.next();
+                }
+            }
+            System.out.println(" ");
+            validacao = false;
 
-            System.out.println(num1 + " __ __ = __");
-            System.out.println("(1) Adição - (2) Subtração - (3) Multiplicação - (4) Divisão");
-            System.out.print("Selecione qual operação deseja nesse calculo: ");
-            int numOperacao = scan.nextInt();
+            while (!validacao) {
+                System.out.println("(1) Adição - (2) Subtração - (3) Multiplicação - (4) Divisão");
+                System.out.print("Selecione qual operação deseja usar nesse calculo: ");
+                if (scan.hasNextInt()) {
+                    operacao = scan.nextInt();
+                    if (operacao > 4 || operacao < 1) {
+                        System.out.println("\n    * O valor inserido é inválido, Você deve selecionar uma das quatro operações (1-4). *\n");
+                    } else {
+                        validacao = true;
+                    }
+                } else {
+                    System.out.println("\n    * O valor inserido é inválido, Você deve selecionar uma das quatro operações (1-4) *\n");
+                    scan.next();
+                }
+            }
 
-            System.out.println(num1 + " " + operacao[numOperacao] + " __ = __");
-            System.out.print("Digite o segundo número: ");
-            int num2 = scan.nextInt();
+            validacao = false;
 
-            switch (numOperacao) {
+            while (!validacao) {
+                System.out.print("Digite o segundo número: ");
+                if (scan.hasNextInt()) {
+                    num2 = scan.nextInt();
+                    validacao = true;
+                } else {
+                    System.out.println("\n    * O valor inserido deve ser um número inteiro *\n");
+                    scan.next();
+                }
+            }
+
+            validacao = false;
+
+            switch (operacao) {
                 case 1:  // Adição
-                    System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+                    System.out.println("\n" + num1 + " + " + num2 + " = " + (num1 + num2));
                     break;
 
                 case 2:  // Subtração
-                    System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+                    System.out.println("\n" + num1 + " - " + num2 + " = " + (num1 - num2));
                     break;
 
                 case 3:  // Multiplicação
-                    System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
+                    System.out.println("\n" + num1 + " x " + num2 + " = " + (num1 * num2));
                     break;
 
                 case 4:  // Divisão
-                    System.out.println(num1 + " ÷ " + num2 + " = " + (num1 / num2));
+                    while (num2 <= 0) {
+                        System.out.println("\n* O segundo número deve ser maior que zero *\n");
+                        while (!validacao) {
+                            System.out.print("Digite o segundo número: ");
+                            if (scan.hasNextInt()) {
+                                num2 = scan.nextInt();
+                                validacao = true;
+                            } else {
+                                System.out.println("\n    * O valor inserido deve ser um número inteiro *\n");
+                                scan.next();
+                            }
+                        }
+                    }
+                    System.out.println("\n" + num1 + " ÷ " + num2 + " = " + (num1 / num2));
                     break;
             }
 
-            System.out.println("Gostaria de fazer um novo calculo? (s/n): ");
+            System.out.print("Gostaria de fazer um novo calculo? (s/n): ");
             repetir = scan.next().charAt(0);
+            while (repetir != 's' && repetir != 'S' && repetir != 'n' && repetir != 'N') {
+                System.out.println("\n    * Resposta inválida *\n");
+                System.out.print("Gostaria de fazer um novo calculo? (s/n): ");
+                repetir = scan.next().charAt(0);
+            }
 
 
-        } while (repetir == 's' || repetir == 'S');
+        }
+        while (repetir == 's' || repetir == 'S');
     }
 }
